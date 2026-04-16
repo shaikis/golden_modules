53c418e507434ee59389564fd81e5d51d4b00d62
Ismail Shaik <60819791+shaikis@users.noreply.github.com>
2026-04-16 13:41:43 +0530

    Add descriptions to outputs in outputs.tf
    Added descriptions for various outputs in outputs.tf to enhance clarity.

--- tf-aws-ec2/outputs.tf
+++ tf-aws-ec2/outputs.tf
@@ -1,4 +1,5 @@
 output "instance_ids" {
+  description = "IDs of on-demand and spot instances."
   value = {
     ondemand = { for k, v in aws_instance.this : k => v.id }
     spot     = { for k, v in aws_spot_instance_request.this : k => v.spot_instance_id }
@@ -6,12 +7,12 @@ output "instance_ids" {
 }
 
 output "instance_arns" {
-  value = {
-    for k, v in aws_instance.this : k => v.arn
-  }
+  description = "ARNs of on-demand instances."
+  value       = { for k, v in aws_instance.this : k => v.arn }
 }
 
 output "private_ips" {
+  description = "Private IPs for all instances."
   value = merge(
     { for k, v in aws_instance.this : k => v.private_ip },
     { for k, v in aws_spot_instance_request.this : k => v.private_ip }
@@ -19,6 +20,7 @@ output "private_ips" {
 }
 
 output "public_ips" {
+  description = "Public IPs for all instances."
   value = merge(
     { for k, v in aws_instance.this : k => v.public_ip },
     { for k, v in aws_spot_instance_request.this : k => v.public_ip }
@@ -26,6 +28,7 @@ output "public_ips" {
 }
 
 output "private_dns" {
+  description = "Private DNS names for all instances."
   value = merge(
     { for k, v in aws_instance.this : k => v.private_dns },
     { for k, v in aws_spot_instance_request.this : k => v.private_dns }
@@ -33,6 +36,7 @@ output "private_dns" {
 }
 
 output "public_dns" {
+  description = "Public DNS names for all instances."
   value = merge(
     { for k, v in aws_instance.this : k => v.public_dns },
     { for k, v in aws_spot_instance_request.this : k => v.public_dns }
@@ -40,6 +44,7 @@ output "public_dns" {
 }
 
 output "availability_zones" {
+  description = "Availability zones for all instances."
   value = merge(
     { for k, v in aws_instance.this : k => v.availability_zone },
     { for k, v in aws_spot_instance_request.this : k => v.availability_zone }
@@ -47,6 +52,7 @@ output "availability_zones" {
 }
 
 output "subnet_ids" {
+  description = "Subnet IDs for all instances."
   value = merge(
     { for k, v in aws_instance.this : k => v.subnet_id },
     { for k, v in aws_spot_instance_request.this : k => v.subnet_id }
@@ -54,5 +60,6 @@ output "subnet_ids" {
 }
 
 output "eip_public_ips" {
-  value = { for k, v in aws_eip.this : k => v.public_ip }
+  description = "Elastic IPs for on-demand instances that requested them."
+  value       = { for k, v in aws_eip.this : k => v.public_ip }
 }
